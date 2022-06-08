00719886a7157888c7e91a63b9a5b343a9fae7f5
EvandroFBL <evandrofbl@gmail.com>
2022-06-07 22:29:15 -0300

    Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { PencilLine} from "phosphor-react"
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+                src="https://images.unsplash.com/photo-1488330890490-c291ecf62571?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+                className={styles.cover}    
+            />
+
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/EvandroFBL.png" />
+                <strong>Evandro Lima</strong>
+                <span>JS Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Edit your profile
+                </a>
+            </footer>
+        </aside>
+    )
+}
